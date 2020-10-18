ecbff2086f10924a6123c5137cd19fb1a33f2bad
John John <fare.zagh@gmail.com>
2020-10-18 14:29:58 +0200

    Api

---
+++ site/JS/temperature.js
@@ -0,0 +1,14 @@
+function AfficherTemperature()
+{
+    let ville="Lyon";
+    let appid="bfb725a2d2eb425c0443cbcdf5c91e8f";
+
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+ville+"&appid="+appid+"&units=metric").then(function(reponse)
+    {
+        json=reponse.json();
+    })
+    .then(function(json)
+    {
+        document.getElementById("temp").innerHTML="La temperature à " +json["name"]+" est de "+json["main"]["temp"]+" °C"
+    })
+}
