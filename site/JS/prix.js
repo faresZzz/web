12bec9f0ef256c8a79e5bfce2063b844236e6ae8
John John <fare.zagh@gmail.com>
2020-10-19 22:08:14 +0200

    ok

--- site/JS/prix.js
+++ site/JS/prix.js
@@ -1,13 +1,14 @@
 
 
-let nbJours=5;
-let nbAdultes=2;
-let nbEnfants=3;
+let nbJours;
+let nbAdultes;
+let nbEnfants;
 let animaux;
 let petitD;
 let prixTotal;
-let datearrive="15/03/2020"
-let datedepart="20/03/2020"
+let depart;
+let arrive;
+let date=Date.now();
 let data={};
 
 function Recup()
@@ -23,20 +24,11 @@ function Recup()
     .then(function(json)
     {   
         data = json;
-        prixAdlt=data[0].prixAdulte;
-        prixEnfant=data[0].prixAdulte*0.4;
-        ville=data[0].ville;
-        pD=data[0].petitDej;
-        if (data[0].animauxOK=="oui"){
-            animaux=data[0].prixAnimaux;
-        }else{
-            animaux=0;
-        }
-
+        
         console.log(data);
         prixTotal=(prixAdlt*nbAdultes+prixEnfant*nbEnfants+animaux+pD)*nbJours;
         console.log(prixTotal);
-        document.getElementById("prix").innerHTML="la reservation de votre hotel pour "+nbJours+" du "+datearrive+" au "+ datedepart+  " est de: "+prixTotal+" $ TTC";
+        document.getElementById("prix").innerHTML="la reservation de votre hotel pour "+nbJours+" jours, du "+depart+" au "+ retour+  " est de: "+prixTotal+" $ TTC";
         
     })  
 }
@@ -44,7 +36,35 @@ function Recup()
 function formulaire()
 {
     nom=document.getElementById("nom").value;
-    console.log(nom);
+    prenom=document.getElementById("prenom").value;
+    mail=document.getElementById("email").value;
+    tel=document.getElementById("telephone").value;
+    depart=document.getElementById("datededepart").value;
+    retour=document.getElementById("datederetour").value;
+    nbAdultes=document.getElementById("nbadulte").value;
+    nbEnfants=document.getElementById("nbenfant").value;
+    pD=document.getElementById("pdej").checked;
+    animaux=document.getElementById("animaux").checked;
+    renseignement=document.getElementById("plus").value;
+
+    console.log(date);
+    
+    if (Date.parse(depart)<date){console.log("vas te faire foutre")}
+    if (Date.parse(retour)<date){console.log("vas te faire foutre")}
+    if (Date.parse(retour)-Date.parse(depart)<0){console.log("tu sais quoi vas te faire foutre")}
+}
+
+function Data()
+{
+    prixAdlt=data[0].prixAdulte;
+    prixEnfant=data[0].prixAdulte*0.4;
+    ville=data[0].ville;
+    pD=data[0].petitDej;
+    if (data[0].animauxOK=="oui"){
+        animaux=data[0].prixAnimaux;
+    }else{
+        animaux=0;
+    }
 
 }
 
